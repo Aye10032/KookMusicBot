2aa1fd4c147938116d07bb9b14e0f116de585e3b
Aye10032 <aye10032@gmail.com>
2023-06-26 01:05:57 +0800

    弃用加密

--- Main.py
+++ Main.py
@@ -26,16 +26,7 @@ def process_json():
     compressed_data = request.data
     uncompressed_data = zlib.decompress(compressed_data)
     parsed_message = json.loads(uncompressed_data.decode('utf-8'))  # 解析JSON字符串
-    # encrypted_content = parsed_message['encrypt']  # 提取密文
-    # print(f'[{encrypted_content}]')
-    #
-    # key = "88MVdZ9"
-    # decryptor = Encrypt(key)
-    # decrypted_content = decryptor.aes_decrypt(encrypted_content)
-    #
-    # print(f'[{decrypted_content}]')
 
-    # data = json.loads(str(decrypted_content))
     print(parsed_message)
     challenge = parsed_message['d']['challenge']
     return jsonify({'challenge': challenge})
