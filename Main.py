155a8f620d07154d8963348377edbc57a18b9911
Aye10032 <aye10032@gmail.com>
2023-06-25 23:26:07 +0800

    提取密文

--- Main.py
+++ Main.py
@@ -25,11 +25,13 @@ class Encrypt:
 def process_json():
     compressed_data = request.data
     uncompressed_data = zlib.decompress(compressed_data)
+    parsed_message = json.loads(uncompressed_data)  # 解析JSON字符串
+    encrypted_content = parsed_message['encrypt']  # 提取密文
 
-    # key = "88MVdZ9"
-    # decryptor = Encrypt(key)
-    # decrypted_content = decryptor.aes_decrypt(uncompressed_data)
-    print(json.dumps(uncompressed_data, indent=4))
+    key = "88MVdZ9"
+    decryptor = Encrypt(key)
+    decrypted_content = decryptor.aes_decrypt(encrypted_content)
+    print(decrypted_content)
     return 'OK'
 
 
