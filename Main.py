b88c7d4db7a2683ba1da1dafd8fa7a7eaf00efe0
Aye10032 <aye10032@gmail.com>
2023-06-25 23:08:19 +0800

    解码

--- Main.py
+++ Main.py
@@ -1,7 +1,22 @@
 from flask import Flask, request
+from Crypto.Cipher import AES
+import base64
 import json
 
 app = Flask(__name__)
+
+
+class Encrypt:
+    def __init__(self, key, bs=32):
+        pad = lambda s: s + (bs - len(s)) * "\0"
+        key = pad(key)
+        self.key = key.encode('utf-8')
+
+    def aes_decrypt(self, content):
+        str = base64.b64decode(content)
+        iv = str[0:16]
+        cipher = AES.new(self.key, AES.MODE_CBC, iv)
+        return cipher.decrypt(base64.b64decode(str[16:])).decode('utf-8')
 
 
 @app.route('/', methods=['POST'])
